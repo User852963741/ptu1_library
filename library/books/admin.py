09770d5d1ed5cb424d912a8359752f52dd071361
Kestutis Januskevicius <infohata@gmail.com>
2022-05-12 16:14:55 +0300

    fixed bookinstance admin create/change view

--- library/books/admin.py
+++ library/books/admin.py
@@ -23,6 +23,7 @@ class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'due_back', 'id', )
     list_filter = ('status', 'due_back', )
     search_fields = ('id', 'book__title', )
+    readonly_fields = ('id', )
 
     fieldsets = (
         ('Pagrindinė Informacija', {'fields': (
